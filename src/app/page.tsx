90a0dc6891e3ca48a27b113e54fb3cf4e135aacf
khawarizmi <khawarizmijefri02@gmail.com>
2024-11-03 22:13:38 +0800

    Refactor page component and remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 
-import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
 import { Button } from "@/components/ui/button";
 
