34c1b59ceb29fba04a6a3e3de60a07ccfdcb764f
khawarizmi <khawarizmijefri02@gmail.com>
2024-11-03 22:05:56 +0800

    Refactor console log in webhook route and remove commented code in post router

--- src/app/api/clerk/webhook/route.ts
+++ src/app/api/clerk/webhook/route.ts
@@ -2,6 +2,6 @@
 
 export const POST = async (req: Request) => {
   const { data } = await req.json();
-  console.log("Received event", event);
+  console.log("Received event", data);
   return new Response("New webhook event received", { status: 200 });
 };
